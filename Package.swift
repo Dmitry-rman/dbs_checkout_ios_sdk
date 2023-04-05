23f32ba8b50e0f5a94bd2953923f118b171a5452
Dmitry Kudryavtsev <dskudryavtsev@gmail.com>
2023-04-05 18:48:07 +0700

    Commit.

--- Package.swift
+++ Package.swift
@@ -22,15 +22,16 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "IPTCheckout"),
+            name: "IPTCheckout",
+            exclude: []),
         .binaryTarget(
             name: "IPTCheckoutCore",
-            url: "https://46009.selcdn.ru/public/iptcheckout/IPTCheckoutCore.xcframework.zip",
+            url: "https://github.com/Dmitry-rman/inplat_checkout_ios_sdk/blob/main/IPTCheckoutCore.xcframework.zip",
             checksum: "0635556eb2b6fa81bbd05429b1f9f9c24d4c731d27b91f8f61df41f84bd1ae69"
         ),
         .binaryTarget(
             name: "IPTCheckoutSDK",
-            url: "https://46009.selcdn.ru/public/iptcheckout/IPTCheckoutSDK.xcframework.zip",
+            url: "https://github.com/Dmitry-rman/inplat_checkout_ios_sdk/blob/main/IPTCheckoutSDK.xcframework.zip",
             checksum: "a86629de04b34220b1bccc13c95171f0616cbee10ef2f03f89cd18480f631133"
         ),
     ]
