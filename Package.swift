9a4dc558a3040bc1a28139dfb481273ed428c876
Dmitry Kudryavtsev <dskudryavtsev@gmail.com>
2023-04-06 14:43:57 +0700

    Commit.

--- Package.swift
+++ Package.swift
@@ -14,12 +14,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "IPTCheckoutCore",
-            url: "https://github.com/Dmitry-rman/inplat_checkout_ios_sdk/releases/download/0.2.5/IPTCheckoutCore.xcframework.zip",
+            url: "https://github.com/Dmitry-rman/inplat_checkout_ios_sdk/releases/download/0.2.6/IPTCheckoutCore.xcframework.zip",
             checksum: "0635556eb2b6fa81bbd05429b1f9f9c24d4c731d27b91f8f61df41f84bd1ae69"
         ),
         .binaryTarget(
             name: "IPTCheckoutSDK",
-            url: "https://github.com/Dmitry-rman/inplat_checkout_ios_sdk/releases/download/0.2.5/IPTCheckoutSDK.xcframework.zip",
+            url: "https://github.com/Dmitry-rman/inplat_checkout_ios_sdk/releases/download/0.2.6/IPTCheckoutSDK.xcframework.zip",
             checksum: "a86629de04b34220b1bccc13c95171f0616cbee10ef2f03f89cd18480f631133"
         )
     ]
