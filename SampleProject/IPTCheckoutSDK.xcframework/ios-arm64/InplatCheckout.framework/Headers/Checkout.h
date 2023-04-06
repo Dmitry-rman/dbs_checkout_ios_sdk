e1ba6b9f6b7978ee480b2e6432ab9a0007c79fb8
Dmitry Kudryavtsev <dskudryavtsev@gmail.com>
2023-04-06 15:39:14 +0700

    Refactoring

---
+++ SampleProject/IPTCheckoutSDK.xcframework/ios-arm64/InplatCheckout.framework/Headers/Checkout.h
@@ -0,0 +1,18 @@
+//
+//  Checkout.h
+//  Checkout
+//
+//  Created by Иван Ерасов on 21.12.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Checkout.
+FOUNDATION_EXPORT double CheckoutVersionNumber;
+
+//! Project version string for Checkout.
+FOUNDATION_EXPORT const unsigned char CheckoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Checkout/PublicHeader.h>
+
+
---
+++ SampleProject/SampleSUIApp/SampleSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleSUIApp.swift
+//  SampleSUIApp
+//
+//  Created by Dmitry on 24.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
